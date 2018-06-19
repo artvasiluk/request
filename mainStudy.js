c8a12ef15b5ef5b28c99cb11fa473816fffc6a0a
Artem Vasiluk <artvasiluk@gmail.com>
2018-06-19 16:19:59 +0300

    before changes in var for finde ""

--- mainStudy.js
+++ mainStudy.js
@@ -14,26 +14,27 @@ function findInWiki(value) {
       if (err) throw err;
 	  	  
 	  var dayBornStart = body.indexOf('Родившиеся ') + 11;
-	  var dayBornFin = body.indexOf('Родившиеся ', dayBornStart + 1) - 2;
+	  var dayBornFin = body.indexOf('Родившиеся ', dayBornStart + 1) - 3;
 	  var yearBornStart = body.indexOf('Родившиеся ', dayBornFin + 1) + 10;
-	  var yearBornFin = body.indexOf('Родившиеся ', yearBornStart + 1) -  2;
+	  var yearBornFin = body.indexOf('Родившиеся ', yearBornStart + 1) -  3;
 	  var cityBornStart = body.indexOf('Родившиеся ', yearBornFin + 1) + 10;
-	  var cityBornFin = body.indexOf('Родившиеся ', cityBornStart + 1) - 2;
+	  var cityBornFin = body.indexOf('Родившиеся ', cityBornStart + 1) - 3;
+	  
 	  var dayDieStart = body.indexOf('Умершие ') + 8;
 	  var dayDieFin = body.indexOf('Умершие ', dayDieStart + 1) - 2;
 	  var yearDieStart = body.indexOf('Умершие ', dayDieFin + 1) + 7;
 	  var yearDieFin = body.indexOf('Умершие ', yearDieStart + 1) - 2;
 	  var cityDieStart = body.indexOf('Умершие ', yearDieFin + 1) + 7;
-	  var cityDieFin = body.indexOf('Умершие ', cityDieStart + 1) - 2;
+	  var cityDieFin = body.indexOf('Умершие ', cityDieStart +1) - 2;
 	  
 	  var dayBorn = body.substring(dayBornStart, dayBornFin);
 	  var yearBorn = body.substring(yearBornStart, yearBornFin);
 	  var cityBorn = body. substring(cityBornStart, cityBornFin);
-	  var dayDie = body.substring(dayBornStart, dayDieFin);
+	  var dayDie = body.substring(dayDieStart, dayDieFin);
 	  var yearDie = body.substring(yearDieStart, yearDieFin);
 	  var cityDie = body.substring(cityDieStart, cityDieFin);
 	  
-	  var result = 'Родился ' + dayBorn + yearBorn + cityBorn + '\n' + 'Умер' + dayDie + yearDie + cityDie;
+	  var result = 'Родился ' + dayBorn + yearBorn; //+ cityBorn + '. Умер' + dayDie + yearDie + cityDie;
 	  
       console.log(result);
 	  // найти когда и где родился и умер и вывести на экран
